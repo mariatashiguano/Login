010f81bb409f320d0abecb93fe4936c1488bb145
mariatashiguano <mariabelen.4021@gmail.com>
2024-05-11 23:18:04 -0500

    cambio a flask

--- connector.py
+++ connector.py
@@ -5,23 +5,26 @@ import mysql.connector
 class login_datos():
 
     def __init__(self):
-        self.connector = mysql.connector.connect( host='localhost',
-                                            database ='loginpd', 
-                                            user = 'root',
-                                            password = '3050123540',
-                                            port = '3306')
+        self.connector = mysql.connector.connect(
+            host='localhost',
+            database='loginpd',
+            user='root',
+            password='3050123540',
+            port='3306'
+        )
+
     def busca_users(self, users):
         cur = self.connector.cursor()
-        sql = "SELECT * FROM login_datos WHERE Users = {}".format(users)
-        cur.execute(sql)
+        sql = "SELECT * FROM login_datos WHERE Users = %s"
+        cur.execute(sql, (users,))
         usersx = cur.fetchall()
-        cur.close()     
+        cur.close()
         return usersx 
 
     def busca_password(self, password):
         cur = self.connector.cursor()
-        sql = "SELECT * FROM login_datos WHERE Password = {}".format(password) #
-        cur.execute(sql)
+        sql = "SELECT * FROM login_datos WHERE Password = %s"
+        cur.execute(sql, (password,))
         passwordx = cur.fetchall()
-        cur.close()     
-        return passwordx 
+        cur.close()
+        return passwordx
